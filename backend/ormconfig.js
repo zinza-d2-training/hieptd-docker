c59c64383d2f841bb51d2b7b8800c73cb392e1d1
tuannd <tuan.nd@zinza.com.vn>
2021-11-18 15:26:17 +0700

    fix config for backend container

--- backend/ormconfig.js
+++ backend/ormconfig.js
@@ -1,11 +1,11 @@
-[
+module.exports = 
   {
-    "type": "mysql",
-    "host": "localhost",
-    "port": 3307,
-    "username": "root",
-    "password": "root",
-    "database": "projectmanager",
+    "type": process.env.DB_CONNECTION,
+    "host": process.env.DB_HOST,
+    "port": process.env.DB_PORT,
+    "username":  process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_DATABASE,
     "entities": ["dist/**/**/*.entity{ .ts,.js}"],
     "synchronize": false,
     "migrations": ["dist/migrations/*{.ts,.js}"],
@@ -15,4 +15,3 @@
       "migrationsDir": "migrations"
     }
   }
-]
